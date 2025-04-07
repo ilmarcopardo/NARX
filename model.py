418fa94abdf7aa90bb24fdb6f4882bfe0fc1a9e6
ilmarcopardo <marco.pardini00@gmail.com>
2025-04-07 17:10:02 +0200

    first test kinda working

--- model.py
+++ model.py
@@ -32,7 +32,7 @@ class NARX(torch.nn.Module):
 
             x_squashed = x[:, i:i+self.d_i, :].view(x.shape[0], self.d_i*self.d_x)
 
-            if y is not None: y_squashed = y_pred[:, i:i+self.d_o, :].view(y.shape[0], self.d_o*self.d_y)
+            if y is not None: y_squashed = y[:, i:i+self.d_o, :].view(y.shape[0], self.d_o*self.d_y)
             else: y_squashed = y_pred[:, i:i+self.d_o, :].view(y_pred.shape[0], self.d_o*self.d_y)
             input_cat = torch.cat((x_squashed, y_squashed), dim=1)
 
@@ -40,22 +40,22 @@ class NARX(torch.nn.Module):
             x_hl = self.act1(x_hl)
 
             x_hl = self.hl2(x_hl)
-            x_hl = self.act2(x_hl)
+            #x_hl = self.act2(x_hl)
 
             y_pred[:, i+self.d_o, :] = x_hl
         
         return y_pred
     
 if __name__ == "__main__":
-    # mode 1
     batch_dim = 1
     x_length = 10
     d_x = 3
     d_y = 1
     x = torch.rand(batch_dim, x_length, d_x)
     model = NARX(d_i = 3, d_o = 3, d_x = d_x, d_y = d_y, d_hl = 8)
-    y = model(x)
 
+    # mode 1
+    y = model(x)
     assert y.shape == (batch_dim, x_length, d_y), f"Unexpected shape: {y.shape}, expected ({batch_dim}, {x_length}, {d_y})"
 
     # mode 2
